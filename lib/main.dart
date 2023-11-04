54318afcb0a5b5c189912352218f9cdc2bafcfeb
Yaseen <syedyaseen1539@gmail.com>
2023-11-04 11:28:33 +0530

    add riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:alhomaidhi_customer_app/src/utils/router/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+
+    ProviderScope(
+      child: MyApp(),
+    ),
+    );
 }
 
 class MyApp extends StatelessWidget {
