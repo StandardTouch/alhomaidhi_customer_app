eb09c4f50618cdb65862c08b26a3967077e782bf
yaseeen96 <syedyaseen1539@gmail.com>
2023-11-08 13:29:58 +0530

    add response model

---
+++ lib/src/features/login/models/send otp/send_otp_response.dart
@@ -0,0 +1,22 @@
+class SendOtpResponseModel {
+  String? message;
+  String? status;
+
+  SendOtpResponseModel({this.message, this.status});
+
+  SendOtpResponseModel.fromJson(Map<String, dynamic> json) {
+    if (json["message"] is String) {
+      message = json["message"];
+    }
+    if (json["status"] is String) {
+      status = json["status"];
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> _data = <String, dynamic>{};
+    _data["message"] = message;
+    _data["status"] = status;
+    return _data;
+  }
+}
