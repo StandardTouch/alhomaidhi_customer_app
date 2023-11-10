a45266db81ee25abe85b2b426b8a7f2e74698d12
yaseeen96 <syedyaseen1539@gmail.com>
2023-11-10 15:50:43 +0530

    replace listtile with usual row convention

--- lib/src/features/my profile/screens/my_profile_screen.dart
+++ lib/src/features/my profile/screens/my_profile_screen.dart
@@ -15,58 +15,56 @@ class MyProfileScreen extends StatefulWidget {
 }
 
 class _MyProfileScreen extends State<MyProfileScreen> {
-  var _width, _height;
-
   var themeMode = false;
 
   @override
   Widget build(context) {
-    _height = DeviceInfo.getDeviceHeight(context);
-    _width = DeviceInfo.getDeviceWidth(context);
+    final height = DeviceInfo.getDeviceHeight(context);
+    final width = DeviceInfo.getDeviceWidth(context);
     ThemeMode themeModeValue = EasyDynamicTheme.of(context).themeMode!;
-    return SingleChildScrollView(
-      child: Container(
-        color: Theme.of(context).scaffoldBackgroundColor,
-        height: _height,
-        width: _width,
-        child: Column(
-          children: [
-            Container(
-              decoration: const BoxDecoration(),
-              height: _height * 0.10,
-              width: _width,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "My Account",
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(
-                top: 50,
-                left: 50,
-                right: 50,
-                bottom: 0,
-              ),
-              decoration: BoxDecoration(
-                color: (themeModeValue == ThemeMode.dark)
-                    ? Colors.black
-                    : Colors.white,
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(50),
-                  topRight: Radius.circular(50),
+    return Container(
+      color: Theme.of(context).scaffoldBackgroundColor,
+      // height: height * 0.8,
+      width: width,
+      child: Column(
+        children: [
+          Container(
+            decoration: const BoxDecoration(),
+            height: height * 0.10,
+            width: width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "My Account",
+                  style: Theme.of(context).textTheme.headlineMedium,
                 ),
-                boxShadow: const [
-                  BoxShadow(
-                      color: Colors.black38, blurRadius: 0.5, spreadRadius: 1),
-                ],
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(
+              top: 50,
+              left: 50,
+              right: 50,
+              bottom: 0,
+            ),
+            decoration: BoxDecoration(
+              color: (themeModeValue == ThemeMode.dark)
+                  ? Colors.black
+                  : Colors.white,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(50),
+                topRight: Radius.circular(50),
               ),
-              height: _height * 0.90,
-              width: _width,
+              boxShadow: const [
+                BoxShadow(
+                    color: Colors.black38, blurRadius: 0.5, spreadRadius: 1),
+              ],
+            ),
+            height: height * 0.82,
+            width: width,
+            child: SingleChildScrollView(
               child: Column(
                 children: [
                   const Image(image: AssetImage(Assets.profile), width: 100),
@@ -87,7 +85,7 @@ class _MyProfileScreen extends State<MyProfileScreen> {
                   const Gap(20),
                   Container(
                     padding: const EdgeInsets.symmetric(
-                      horizontal: 25,
+                      horizontal: 0,
                       vertical: 10,
                     ),
                     decoration: BoxDecoration(
@@ -97,36 +95,62 @@ class _MyProfileScreen extends State<MyProfileScreen> {
                       ),
                       borderRadius: BorderRadius.circular(10),
                     ),
-                    child: Row(
-                      children: [
-                        const Image(
-                          image: AssetImage(Assets.themeMode),
-                          width: 33,
+                    child: ListTile(
+                      leading: const Image(
+                        image: AssetImage(Assets.themeMode),
+                        width: 33,
+                      ),
+                      title: Text("Theme"),
+                      trailing: Transform.scale(
+                        scale: 0.8,
+                        child: Switch(
+                          value: themeMode,
+                          onChanged: (value) {
+                            setState(() {
+                              themeMode = value;
+                              if (themeMode) {
+                                EasyDynamicTheme.of(context)
+                                    .changeTheme(dynamic: false, dark: true);
+                              } else {
+                                EasyDynamicTheme.of(context)
+                                    .changeTheme(dynamic: true, dark: false);
+                              }
+                            });
+                          },
+                          activeColor: Theme.of(context).primaryColor,
                         ),
-                        const Gap(20),
-                        const Text("Theme"),
-                        const Gap(30),
-                        Transform.scale(
-                          scale: 0.8,
-                          child: Switch(
-                            value: themeMode,
-                            onChanged: (value) {
-                              setState(() {
-                                themeMode = value;
-                                if (themeMode) {
-                                  EasyDynamicTheme.of(context)
-                                      .changeTheme(dynamic: false, dark: true);
-                                } else {
-                                  EasyDynamicTheme.of(context)
-                                      .changeTheme(dynamic: true, dark: false);
-                                }
-                              });
-                            },
-                            activeColor: Theme.of(context).primaryColor,
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
+                    // child: Row(
+                    //   children: [
+                    //     const Image(
+                    //       image: AssetImage(Assets.themeMode),
+                    //       width: 33,
+                    //     ),
+                    //     const Gap(20),
+                    //     const Text("Theme"),
+                    //     const Gap(30),
+                    // Transform.scale(
+                    //   scale: 0.8,
+                    //   child: Switch(
+                    //     value: themeMode,
+                    //     onChanged: (value) {
+                    //       setState(() {
+                    //         themeMode = value;
+                    //         if (themeMode) {
+                    //           EasyDynamicTheme.of(context)
+                    //               .changeTheme(dynamic: false, dark: true);
+                    //         } else {
+                    //           EasyDynamicTheme.of(context)
+                    //               .changeTheme(dynamic: true, dark: false);
+                    //         }
+                    //       });
+                    //     },
+                    //     activeColor: Theme.of(context).primaryColor,
+                    //   ),
+                    // ),
+                    //   ],
+                    // ),
                   ),
                   const Gap(20),
                   const MyAccountMenuItems(
@@ -136,7 +160,7 @@ class _MyProfileScreen extends State<MyProfileScreen> {
                   ),
                   const Gap(50),
                   SizedBox(
-                    width: _width,
+                    width: width,
                     child: ElevatedButton(
                       onPressed: () {},
                       child: const Text("Logout"),
@@ -145,8 +169,8 @@ class _MyProfileScreen extends State<MyProfileScreen> {
                 ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
