7e6bd37bd3011b29f043f07eee4e8746f00b65bc
zaid2229 <zzaidu2018@gmail.com>
2023-12-01 17:36:33 +0530

    modify order provider

--- lib/src/features/my profile/features/my_orders/providers/orders_provider.dart
+++ lib/src/features/my profile/features/my_orders/providers/orders_provider.dart
@@ -1,4 +1,4 @@
-import 'package:alhomaidhi_customer_app/src/features/my%20profile/features/my_orders/models/my_order_detals_model.dart';
+import 'package:alhomaidhi_customer_app/src/features/my%20profile/features/my_orders/models/my_order_details_model.dart';
 import 'package:alhomaidhi_customer_app/src/features/my%20profile/features/my_orders/models/my_orders_model.dart';
 import 'package:alhomaidhi_customer_app/src/features/my%20profile/features/my_orders/services/my_order_details_services.dart';
 import 'package:alhomaidhi_customer_app/src/features/my%20profile/features/my_orders/services/order_services.dart';
