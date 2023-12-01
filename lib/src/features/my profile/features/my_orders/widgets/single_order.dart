fafc175297ebf0507043086adbcb1bd93dc904bb
silent47hm <maazm7391@gmail.com>
2023-12-01 20:08:25 +0530

    Add structureof widget for single order card

--- lib/src/features/my profile/features/my_orders/widgets/single_order.dart
+++ lib/src/features/my profile/features/my_orders/widgets/single_order.dart
@@ -1,12 +1,65 @@
 import 'package:flutter/material.dart';
 
 class SingleOrderCard extends StatelessWidget {
-  const SingleOrderCard({super.key});
-// @maaz - Add your code here....
+  var borderRadius = const BorderRadius.only(
+    topRight: Radius.circular(10),
+    bottomRight: Radius.circular(10),
+    bottomLeft: Radius.circular(10),
+    topLeft: Radius.circular(10),
+  );
+  SingleOrderCard({super.key});
+//  https://alhomaidhigroup.com/wp-content/uploads/2023/11/dStvdWIxbkFadEYvNHJQem94SktpQT09.png
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Single Order Card"),
+      height: 90,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.black),
+          borderRadius: borderRadius),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            padding: EdgeInsets.all(4),
+            width: 82,
+            height: 82,
+            child: Image.network(
+                "https://alhomaidhigroup.com/wp-content/uploads/2023/11/dStvdWIxbkFadEYvNHJQem94SktpQT09.png"),
+          ),
+          Container(
+            width: double.infinity,
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Text("title"), Text("Subtitle")],
+            ),
+          ),
+          Icon(Icons.arrow_forward_ios)
+        ],
+      ),
     );
+
+    // return Container(
+    //   height: 85,
+    //   child: ListTile(
+    //     trailing: Icon(Icons.arrow_forward_ios),
+    //     tileColor: Colors.white,
+    //     shape: RoundedRectangleBorder(
+    //       borderRadius: borderRadius,
+    //       side: const BorderSide(
+    //         width: 1,
+    //         color: Colors.black,
+    //       ),
+    //     ),
+    //     leading: Image.network(
+    //       "https://alhomaidhigroup.com/wp-content/uploads/2023/11/dStvdWIxbkFadEYvNHJQem94SktpQT09.png",
+    //       width: 82,
+    //       height: 82,
+    //     ),
+    //     title: const Text("Delivered on Mon Oct 25"),
+    //     subtitle: const Text("Calvin Klein  /  كلفين كلاين"),
+    //   ),
+    // );
   }
 }
