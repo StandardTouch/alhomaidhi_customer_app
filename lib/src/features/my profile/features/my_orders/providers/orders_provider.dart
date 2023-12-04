3f852f8e41979226e5a65e64436880ac876bac13
zaid2229 <zzaidu2018@gmail.com>
2023-12-04 18:06:08 +0530

    del my_order_details model

--- lib/src/features/my profile/features/my_orders/providers/orders_provider.dart
+++ lib/src/features/my profile/features/my_orders/providers/orders_provider.dart
@@ -1,4 +1,3 @@
-import 'package:alhomaidhi_customer_app/src/features/my%20profile/features/my_orders/models/my_order_details_model.dart';
 import 'package:alhomaidhi_customer_app/src/features/my%20profile/features/my_orders/models/my_order_details_models.dart';
 import 'package:alhomaidhi_customer_app/src/features/my%20profile/features/my_orders/models/my_orders_model.dart';
 import 'package:alhomaidhi_customer_app/src/features/my%20profile/features/my_orders/services/my_order_details_services.dart';
@@ -10,7 +9,8 @@ final MyOrdersProvider = FutureProvider<MyOrdersModel>((ref) async {
   return response;
 });
 
-final MyOrderDetailsProvider = FutureProvider<MyOrderDetailsModels>((ref) async {
+final MyOrderDetailsProvider =
+    FutureProvider<MyOrderDetailsModels>((ref) async {
   MyOrderDetailsModels response = await getMyOrderDetails();
   return response;
 });
