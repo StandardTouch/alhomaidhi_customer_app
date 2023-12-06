fd826177fb6fb25b44662a4d88fb5720a587c117
zaid2229 <zzaidu2018@gmail.com>
2023-12-06 13:04:17 +0530

    add my orders list integration

--- lib/src/features/my profile/features/my_orders/screens/my_order_details.dart
+++ lib/src/features/my profile/features/my_orders/screens/my_order_details.dart
@@ -1,9 +1,6 @@
-import 'package:alhomaidhi_customer_app/src/features/login/models/login_response.dart';
 import 'package:alhomaidhi_customer_app/src/features/my%20profile/features/my_orders/providers/orders_provider.dart';
-import 'package:alhomaidhi_customer_app/src/utils/constants/endpoints.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:logger/logger.dart';
 
 class MyOrderDetailsScreen extends ConsumerWidget {
   const MyOrderDetailsScreen({super.key});
@@ -12,12 +9,12 @@ class MyOrderDetailsScreen extends ConsumerWidget {
     final myOrderDetail = ref.watch(MyOrderDetailsProvider);
     return myOrderDetail.when(data: (data) {
       if (data.status == "APP00") {
-      // logger.i(data.message!.toString());
+        // logger.i(data.message!.toString());
         return Center(
           child: Text(data.message!.orderDetails!.cartTax!),
         );
       } else {
-        return Center(
+        return const Center(
           child: Text("data.errorMessage!"),
         );
       }
@@ -26,8 +23,8 @@ class MyOrderDetailsScreen extends ConsumerWidget {
         child: Text("$err"),
       );
     }, loading: () {
-      return Center(
-        child: const CircularProgressIndicator(),
+      return const Center(
+        child: CircularProgressIndicator(),
       );
     });
   }
