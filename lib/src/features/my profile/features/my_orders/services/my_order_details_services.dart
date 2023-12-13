058181007d48bcf52695d68aa07c095fda33085c
zaid2229 <zzaidu2018@gmail.com>
2023-12-13 11:23:45 +0530

    add optimized code

--- lib/src/features/my profile/features/my_orders/services/my_order_details_services.dart
+++ lib/src/features/my profile/features/my_orders/services/my_order_details_services.dart
@@ -3,18 +3,15 @@ import 'package:alhomaidhi_customer_app/src/utils/config/dio/dio_client.dart';
 import 'package:alhomaidhi_customer_app/src/utils/constants/endpoints.dart';
 import 'package:alhomaidhi_customer_app/src/utils/helpers/auth_helper.dart';
 import 'package:dio/dio.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 Future<MyOrderDetailsModels> getMyOrderDetails(String? orderId) async {
   final authDetails = await AuthHelper.getAuthDetails();
-  print('this is orderid $orderId');
   String cleanOrderId(String? orderId) {
-    return orderId!.replaceFirst(
-        '#', ''); // Removes the first occurrence of '#' from the string
+    return orderId!.replaceFirst('#', '');
   }
 
   String? cleanedOrderId = cleanOrderId(orderId);
-  print('this is cleanedorderid $cleanedOrderId');
+
   try {
     final jsonResponse = await dioClient.get(APIEndpoints.getSingleOrder,
         options: Options(headers: {
@@ -25,11 +22,10 @@ Future<MyOrderDetailsModels> getMyOrderDetails(String? orderId) async {
     final response = MyOrderDetailsModels.fromJson(
       jsonResponse.data,
     );
-
-    print(jsonResponse);
     return response;
   } catch (err) {
     logger.e("Error from getMyOrders: $err");
+
     throw Exception("$err");
   }
 }
