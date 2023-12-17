2fe5aa377d8add9f641a43099a8ceed67822565e
yaseeen96 <syedyaseen1539@gmail.com>
2023-12-17 13:32:57 +0530

    add base screen for my cart

--- lib/src/features/cart/screens/shopping_cart.dart
+++ lib/src/features/cart/screens/shopping_cart.dart
@@ -1,3 +1,5 @@
+import 'package:alhomaidhi_customer_app/src/features/cart/providers/my_cart_provider.dart';
+import 'package:alhomaidhi_customer_app/src/utils/exceptions/homaidhi_exception.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -11,6 +13,31 @@ class ShoppingCartScreen extends ConsumerStatefulWidget {
 class _ShoppingCartScreenState extends ConsumerState<ShoppingCartScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final cart = ref.watch(myCartProvider);
+    return Scaffold(
+      appBar: AppBar(
+        forceMaterialTransparency: true,
+        title: const Text("My Cart"),
+      ),
+      body: cart.when(data: (data) {
+        return const Center(
+          child: Text("Data found"),
+        );
+      }, error: (err, stk) {
+        if (err is HomaidhiException) {
+          return Center(
+            child: Text(err.message),
+          );
+        } else {
+          return Center(
+            child: Text(err.toString()),
+          );
+        }
+      }, loading: () {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      }),
+    );
   }
 }
