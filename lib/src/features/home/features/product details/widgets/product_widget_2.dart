c5b0303ab5b84cad9e67b839270ecb90fdbefee9
yaseeen96 <syedyaseen1539@gmail.com>
2023-12-19 19:54:19 +0530

    add address in product detail screen

--- lib/src/features/home/features/product details/widgets/product_widget_2.dart
+++ lib/src/features/home/features/product details/widgets/product_widget_2.dart
@@ -1,10 +1,29 @@
+import 'package:alhomaidhi_customer_app/src/utils/helpers/address_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
 
-class ProductWidget2 extends StatelessWidget {
+class ProductWidget2 extends ConsumerStatefulWidget {
   const ProductWidget2({
     super.key,
   });
+
+  @override
+  ConsumerState<ProductWidget2> createState() => _ProductWidget2State();
+}
+
+class _ProductWidget2State extends ConsumerState<ProductWidget2> {
+  late Map<String, String>? address;
+
+  void fetchAddress() async {
+    address = await getAddressData(ref);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchAddress();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,49 +35,59 @@ class ProductWidget2 extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              RichText(
-                text: TextSpan(
-                    text: "Delivered to ",
-                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                          color: Colors.grey,
+          Expanded(
+            flex: 2,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                RichText(
+                  text: TextSpan(
+                      text: "Delivered to ",
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: Colors.grey,
+                          ),
+                      children: [
+                        TextSpan(
+                          text: address?["name"] ?? "No Name provided",
+                          style:
+                              Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                    color: Colors.black,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
                         ),
-                    children: [
-                      TextSpan(
-                        text: "Mr. Abdul Malik",
-                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                              color: Colors.black,
-                            ),
-                      ),
-                    ]),
-              ),
-              const Gap(5),
-              Text(
-                "Customer Address, KSA",
-                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      color: Colors.grey,
-                    ),
-              )
-            ],
-          ),
-          Material(
-            child: InkWell(
-              borderRadius: BorderRadius.circular(10),
-              splashColor: Theme.of(context).primaryColor.withOpacity(0.2),
-              onTap: () {},
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black,
-                  ),
-                  borderRadius: BorderRadius.circular(10),
+                      ]),
                 ),
-                alignment: Alignment.center,
-                height: 40,
-                width: 100,
-                child: Text("Change"),
+                const Gap(5),
+                Text(
+                  address?["address"] ?? "No Address Provided",
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Colors.grey,
+                      ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Material(
+              child: InkWell(
+                borderRadius: BorderRadius.circular(10),
+                splashColor: Theme.of(context).primaryColor.withOpacity(0.2),
+                onTap: () {},
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.black,
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  alignment: Alignment.center,
+                  height: 40,
+                  width: 100,
+                  child: Text("Change"),
+                ),
               ),
             ),
           )
