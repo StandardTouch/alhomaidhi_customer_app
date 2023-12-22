71781535f9587ebce7f1aa8119ba23a25d9babc4
zaid2229 <zzaidu2018@gmail.com>
2023-12-22 20:35:21 +0530

    add to solve bug

--- lib/src/features/my profile/features/my_invoices/widgets/my_invoice.dart
+++ lib/src/features/my profile/features/my_invoices/widgets/my_invoice.dart
@@ -1,12 +1,16 @@
 import 'dart:isolate';
 import 'dart:ui';
+import 'package:background_downloader/background_downloader.dart'
+    as BackgroundDownloader;
 
+import 'package:background_downloader/src/permissions.dart';
 import 'package:alhomaidhi_customer_app/src/utils/constants/endpoints.dart';
 import 'package:alhomaidhi_customer_app/src/utils/helpers/device_info.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:logger/logger.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:permission_handler/permission_handler.dart' as Permissions;
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
@@ -16,8 +20,7 @@ FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
 
 class MyInvoice extends StatefulWidget {
-  MyInvoice({super.key, required this.invoiceId, required this.invoicePdf});
-
+  MyInvoice({key, required this.invoiceId, required this.invoicePdf});
   final String? invoiceId;
   final String? invoicePdf;
 
@@ -27,143 +30,34 @@ class MyInvoice extends StatefulWidget {
 
 class _MyInvoiceState extends State<MyInvoice> {
   bool _isDownloading = false;
+  String? _currentTaskId;
   final ReceivePort _port = ReceivePort();
-  final borderRadius = const BorderRadius.only(
-    topRight: Radius.circular(10),
-    bottomRight: Radius.circular(10),
-    bottomLeft: Radius.circular(10),
-    topLeft: Radius.circular(10),
-  );
-
-// downloads pdf
-  Future<void> getMyInvoicePdf(String url, String fileName) async {
-    if (_isDownloading) {
-      // A download is already in progress
-      return;
-    }
-    setState(() {
-      _isDownloading = true;
-    });
-    final plugin = DeviceInfoPlugin();
-    final android = await plugin.androidInfo;
-    try {
-      final storageStatus = android.version.sdkInt < 33
-          ? await Permission.storage.request()
-          : PermissionStatus.granted;
-      // Requesting storage permission
-      // var status = await Permission.storage.request();
-      logger.i(storageStatus);
-      if (storageStatus == PermissionStatus.granted) {
-        // Getting the external storage directory
-        Directory? directory = await getExternalStorageDirectory();
-        String newPath = "";
-        List<String> paths = directory!.path.split("/");
-        for (int x = 1; x < paths.length; x++) {
-          String folder = paths[x];
-          if (folder != "Android") {
-            newPath += "/" + folder;
-          } else {
-            break;
-          }
-        }
-        newPath = newPath + "/Download";
-        directory = Directory(newPath);
-
-        // The file's path where it will be saved
-        final filePath = '${directory.path}';
-        if (!url.startsWith('http://') && !url.startsWith('https://')) {
-          url = 'https:' + url;
-        }
-        // Downloading using Dio
-        // var dio = Dio();
-        // await dio.download(url, filePath, onReceiveProgress: (received, total) {
-        //   if (total != -1) {
-        //     // Calculate the progress percentage
-        //     int progress = ((received / total) * 100).toInt();
-        //     // Call a function to update the notification with the current progress
-        //     showDownloadNotification(progress, false);
-        //   }
-        // });
-
-        final pdfname = '${widget.invoiceId}.pdf';
-        logger.i(url);
-        final PermissionStatus status = await Permission.notification.request();
-
-        if (status.isGranted) {
-          try {
-            await FlutterDownloader.enqueue(
-                url: url,
-                savedDir: filePath,
-                fileName: pdfname,
-                showNotification: true,
-                saveInPublicStorage: true,
-                openFileFromNotification: true,
-                timeout: 90000);
-
-            logger.i('Downloaded file path: $filePath');
-          } catch (err) {
-            setState(() {
-              _isDownloading = false;
-            });
-            logger.e(err);
-          }
-        }
-      } else {
-        logger.e('No storage permission granted.');
-      }
-    } catch (e) {
-      logger.e('Error downloading file: $e');
-    }
-  }
-
-  @pragma('vm:entry-point')
-  static void downloadCallback(String id, int status, int progress) {
-    final SendPort? send =
-        IsolateNameServer.lookupPortByName('downloader_send_port');
-    send!.send([id, status, progress]);
-  }
-
-  void _updateDownloadState(DownloadTaskStatus status) {
-    logger.d("status from function $status");
-    if (status == DownloadTaskStatus.complete) {
-      setState(() {
-        _isDownloading = false;
-      });
-    }
-  }
+  final borderRadius = const BorderRadius.all(Radius.circular(10));
 
   @override
   void initState() {
     super.initState();
+    FlutterDownloader.registerCallback(downloadCallback);
+  }
 
+  bool _initDownloaderListener() {
+    bool _isSuccess = false;
+    logger.e('inside the iniit dowmloader');
     IsolateNameServer.registerPortWithName(
         _port.sendPort, 'downloader_send_port');
-    _port.listen(
-      (dynamic data) {
-        String id = data[0];
-        DownloadTaskStatus status = DownloadTaskStatus.fromInt(data[1]);
-
-        int progress = data[2];
-
-        logger.e(status);
-        _updateDownloadState(status);
-
-        // setState(() {
-
-        // });
-      },
-      onDone: () {
-        logger.e("completed");
-        setState(
-          () {
-            _isDownloading = false;
-          },
-        );
-      },
-    );
-    logger.e("called outside function");
-
-    FlutterDownloader.registerCallback(downloadCallback);
+    _port.listen((dynamic data) {
+      String id = data[0];
+      Enum status = DownloadTaskStatus.fromInt(data[1]);
+      logger.e(status);
+      logger.i(status == DownloadTaskStatus.complete);
+      if (status == DownloadTaskStatus.complete) {
+        logger.e('from checking status $_isDownloading');
+        _isSuccess = true;
+      } else {
+        _isSuccess = false;
+      }
+    });
+    return _isSuccess;
   }
 
   @override
@@ -172,15 +66,88 @@ class _MyInvoiceState extends State<MyInvoice> {
     super.dispose();
   }
 
+  Future<void> getMyInvoicePdf(String url, String fileName) async {
+    logger.e('from get my invoice function $_isDownloading');
+    setState(() => _isDownloading = true);
+
+    final plugin = DeviceInfoPlugin();
+    final android = await plugin.androidInfo;
+    try {
+      final Enum storageStatus = android.version.sdkInt < 33
+          ? await Permissions.Permission.storage.request()
+          : PermissionStatus.granted;
+      logger.i(storageStatus);
+      logger.i(PermissionStatus.granted);
+      logger.i(storageStatus == Permissions.PermissionStatus.granted);
+      if (storageStatus == Permissions.PermissionStatus.granted) {
+        Directory? directory = await getExternalStorageDirectory();
+        String newPath = "${directory!.path}/Download";
+        Directory downloadDirectory = Directory(newPath);
+        if (!await downloadDirectory.exists()) {
+          await downloadDirectory.create(recursive: true);
+        }
+        final filePath = "$newPath/";
+        if (!url.startsWith('http://') && !url.startsWith('https://')) {
+          url = 'https:' + url;
+        }
+        final pdfname = '${widget.invoiceId}.pdf';
+        final Permissions.PermissionStatus status =
+            await Permissions.Permission.notification.request();
+        if (status.isDenied) {
+          return;
+        }
+        final taskId = await FlutterDownloader.enqueue(
+            url: url,
+            savedDir: filePath,
+            fileName: pdfname,
+            showNotification: true,
+            openFileFromNotification: true,
+            saveInPublicStorage: true);
+        _currentTaskId = taskId;
+        final isSuccess = _initDownloaderListener();
+        if (isSuccess) {
+          setState(() {
+            _isDownloading = false;
+          });
+        } else {
+          setState(() {
+            _isDownloading = true;
+          });
+        }
+      } else {
+        setState(
+          () => _isDownloading = false,
+        );
+        // Consider showing an alert to the user that the permission is not granted
+        logger.e('No storage permission granted.');
+        return;
+      }
+    } catch (e) {
+      logger.e('Error downloading file: $e');
+      setState(
+        () => _isDownloading = false,
+      );
+    }
+  }
+
+  @pragma('vm:entry-point')
+  static void downloadCallback(String id, int status, int progress) async {
+    final SendPort? send =
+        IsolateNameServer.lookupPortByName('downloader_send_port');
+    send?.send([id, status, progress]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {
-        if (!_isDownloading) {
-          getMyInvoicePdf(widget.invoicePdf!, widget.invoiceId!);
-        }
-      },
+      onTap: _isDownloading
+          ? null
+          : () {
+              logger.i('inside ontop function $_isDownloading');
+              getMyInvoicePdf(widget.invoicePdf!, widget.invoiceId!);
+            },
       child: Container(
+        key: ValueKey(_isDownloading),
         margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 8),
         height: DeviceInfo.getDeviceHeight(context) * 0.1,
         decoration: BoxDecoration(
@@ -188,8 +155,15 @@ class _MyInvoiceState extends State<MyInvoice> {
           borderRadius: borderRadius,
         ),
         child: _isDownloading
-            ? CircularProgressIndicator()
+            ? Center(
+                key: ValueKey(_isDownloading),
+                child: SizedBox(
+                    width: 50.0,
+                    height: 50.0,
+                    child: CircularProgressIndicator()),
+              )
             : Row(
+                key: ValueKey(_isDownloading),
                 mainAxisAlignment: MainAxisAlignment.start,
                 mainAxisSize: MainAxisSize.min,
                 children: [
@@ -214,14 +188,6 @@ class _MyInvoiceState extends State<MyInvoice> {
                             style: Theme.of(context).textTheme.bodyMedium!,
                             overflow: TextOverflow.ellipsis,
                           ),
-                          //  FittedBox(
-                          //     child: Text(
-                          //        'Calvin Klein  /  كلفين كلاين',
-                          //       style: Theme.of(context).textTheme.labelMedium,
-                          //       // overflow: TextOverflow.ellipsis,
-                          //       maxLines: 2,
-                          //     ),
-                          //   )
                         ],
                       ),
                     ),
