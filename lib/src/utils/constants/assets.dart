9840d34409c38acfd16ffe5145c205d5dd6def45
yaseeen96 <syedyaseen1539@gmail.com>
2024-01-19 19:03:19 +0530

    add splash screen and app icon

--- lib/src/utils/constants/assets.dart
+++ lib/src/utils/constants/assets.dart
@@ -3,18 +3,13 @@ class Assets {
   static const logoDark = "assets/images/alhomaidhi-dark.png";
   static const profile = "assets/images/profile.png";
   static const myOrders = "assets/images/my_orders.png";
-  static const themeMode = "assets/images/theme_mode.png";
   static const delete = "assets/images/delete.png";
   static const sort = "assets/images/sort_icon.png";
   static const brandsButton = "assets/images/brands_button.png";
   static const placeHolderImage = "assets/images/placeholder_image.png";
   static const invoice = "assets/images/Cheque.png";
   static const protectIcon = "assets/images/Protect.png";
-  static const tamaraIcon = "assets/images/EN Colored widget.png";
-  static const tabbyIcon = "assets/images/tabby_logo.png";
-  static const hyperPayLogo = "assets/images/Hyperpay-logo.png";
   static const thumbsUpIcon = "assets/images/thumbs_up.jpg";
-
   static const fallBackProductImage =
       "https://alhomaidhigroup.com/wp-content/uploads/2023/12/no-image-found-1.png";
   static const errorImage = "assets/images/error.png";
