c354c808fd65e90fa95daedcdd2d7f9aa99bc218
yaseeen96 <syedyaseen1539@gmail.com>
2024-01-22 15:42:57 +0530

    update profile screen

--- lib/src/features/my profile/features/profile/screens/my_profile_screen.dart
+++ lib/src/features/my profile/features/profile/screens/my_profile_screen.dart
@@ -32,101 +32,225 @@ class _MyProfileScreen extends State<MyProfileScreen> {
   @override
   Widget build(context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "My Account",
-          style: Theme.of(context).textTheme.headlineMedium,
-        ),
-        forceMaterialTransparency: true,
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          const Gap(30),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.only(
-                top: 50,
-                left: 50,
-                right: 50,
-                bottom: 0,
-              ),
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(25),
-                  topRight: Radius.circular(25),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                      color: Colors.black38, blurRadius: 0.5, spreadRadius: 1),
-                ],
-              ),
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    CircleAvatar(
-                      radius: 60,
-                      backgroundColor: Theme.of(context).primaryColor,
-                      child: const Icon(
-                        Icons.account_circle,
-                        size: 120,
-                      ),
-                    ),
-                    const Gap(20),
-                    FutureBuilder(
-                        future: getUserName(),
-                        builder: (context, snapshot) {
-                          return Text("Hey, ${snapshot.data}");
-                        }),
-                    const Gap(20),
-                    const MyProfiletMenuItem(
-                      menuItemLink: '/address',
-                      menuitemName: "Billing Address",
-                      menuItemImage: Assets.profile,
-                      additionalWidget:
-                          Icon(Icons.keyboard_arrow_right_rounded),
-                    ),
-                    const Gap(20),
-                    const MyProfiletMenuItem(
-                      menuItemLink: '/my_orders',
-                      menuitemName: "My Orders",
-                      menuItemImage: Assets.myOrders,
-                      additionalWidget:
-                          Icon(Icons.keyboard_arrow_right_rounded),
-                    ),
-                    const Gap(20),
-                    const MyProfiletMenuItem(
-                      menuItemLink: '/my_invoices',
-                      menuitemName: "My Invoices",
-                      menuItemImage: Assets.invoice,
-                      additionalWidget:
-                          Icon(Icons.keyboard_arrow_right_rounded),
-                    ),
-                    const Gap(20),
-                    const MyProfiletMenuItem(
-                      menuItemLink: '/delete_profile',
-                      menuitemName: "Delete Account",
-                      menuItemImage: Assets.delete,
-                      additionalWidget:
-                          Icon(Icons.keyboard_arrow_right_rounded),
-                    ),
-                    const Gap(30),
-                    SizedBox(
-                      width: DeviceInfo.getDeviceWidth(context),
-                      child: ElevatedButton(
-                        onPressed: logout,
-                        child: const Text("Logout"),
-                      ),
-                    ),
-                    const Gap(100),
-                  ],
-                ),
-              ),
-            ),
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          title: Text(
+            "My Profile",
+            style: Theme.of(context)
+                .textTheme
+                .headlineMedium!
+                .copyWith(color: Colors.white),
           ),
-        ],
-      ),
-    );
+          forceMaterialTransparency: true,
+          centerTitle: true,
+          actions: [
+            IconButton(
+                onPressed: logout,
+                icon: const Icon(
+                  Icons.logout,
+                  color: Colors.white,
+                ))
+          ],
+        ),
+        body: Stack(
+          children: [
+            Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10),
+                    ),
+                  ),
+                  height: DeviceInfo.getDeviceHeight(context) * 0.2,
+                ),
+                Gap(DeviceInfo.getDeviceHeight(context) * 0.15),
+                const Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: MyProfileMenuItem(
+                              menuItemLink: "/address",
+                              menuitemName: "My Address",
+                              icon: Icons.location_city,
+                            ),
+                          ),
+                          Gap(10),
+                          Expanded(
+                            flex: 1,
+                            child: MyProfileMenuItem(
+                              menuItemLink: "/my_orders",
+                              menuitemName: "My Orders",
+                              icon: Icons.list,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Gap(10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: MyProfileMenuItem(
+                              menuItemLink: "/my_invoices",
+                              menuitemName: "My Invoices",
+                              icon: Icons.receipt_outlined,
+                            ),
+                          ),
+                          Gap(10),
+                          Expanded(
+                            flex: 1,
+                            child: MyProfileMenuItem(
+                              isDeleteAccount: true,
+                              menuItemLink: "/delete_profile",
+                              menuitemName: "Delete Account",
+                              icon: Icons.delete_forever,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+
+            // profile card
+            Positioned(
+                right: 0,
+                left: 0,
+                top: DeviceInfo.getDeviceHeight(context) * 0.2 / 2,
+                child: Container(
+                  margin: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.onPrimary,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          color: Colors.black.withOpacity(0.3))
+                    ],
+                  ),
+                  height: DeviceInfo.getDeviceHeight(context) * 0.2,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: CircleAvatar(
+                          radius: 30,
+                          backgroundColor: Theme.of(context).primaryColor,
+                          child: const Icon(
+                            Icons.account_circle,
+                            size: 50,
+                          ),
+                        ),
+                      ),
+                      FutureBuilder(
+                          future: getUserName(),
+                          builder: (context, snapshot) {
+                            return Text(
+                              "Hello, ${snapshot.data}",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headlineMedium!
+                                  .copyWith(
+                                    color: Theme.of(context).primaryColor,
+                                  ),
+                            );
+                          }),
+                    ],
+                  ),
+                ))
+          ],
+        )
+
+        //  Expanded(
+        //   child: Container(
+        //     padding: const EdgeInsets.only(
+        //       top: 50,
+        //       left: 50,
+        //       right: 50,
+        //       bottom: 0,
+        //     ),
+        //     decoration: const BoxDecoration(
+        //       color: Colors.white,
+        //       borderRadius: BorderRadius.only(
+        //         topLeft: Radius.circular(25),
+        //         topRight: Radius.circular(25),
+        //       ),
+        //       boxShadow: [
+        //         BoxShadow(
+        //             color: Colors.black38, blurRadius: 0.5, spreadRadius: 1),
+        //       ],
+        //     ),
+        //     child: SingleChildScrollView(
+        //       child: Column(
+        //         children: [
+        //           CircleAvatar(
+        //             radius: 60,
+        //             backgroundColor: Theme.of(context).primaryColor,
+        //             child: const Icon(
+        //               Icons.account_circle,
+        //               size: 120,
+        //             ),
+        //           ),
+        //           const Gap(20),
+        //           FutureBuilder(
+        //               future: getUserName(),
+        //               builder: (context, snapshot) {
+        //                 return Text("Hey, ${snapshot.data}");
+        //               }),
+        //           const Gap(20),
+        //           const MyProfiletMenuItem(
+        //             menuItemLink: '/address',
+        //             menuitemName: "Billing Address",
+        //             menuItemImage: Assets.profile,
+        //             additionalWidget: Icon(Icons.keyboard_arrow_right_rounded),
+        //           ),
+        //           const Gap(20),
+        //           const MyProfiletMenuItem(
+        //             menuItemLink: '/my_orders',
+        //             menuitemName: "My Orders",
+        //             menuItemImage: Assets.myOrders,
+        //             additionalWidget: Icon(Icons.keyboard_arrow_right_rounded),
+        //           ),
+        //           const Gap(20),
+        //           const MyProfiletMenuItem(
+        //             menuItemLink: '/my_invoices',
+        //             menuitemName: "My Invoices",
+        //             menuItemImage: Assets.invoice,
+        //             additionalWidget: Icon(Icons.keyboard_arrow_right_rounded),
+        //           ),
+        //           const Gap(20),
+        //           const MyProfiletMenuItem(
+        //             menuItemLink: '/delete_profile',
+        //             menuitemName: "Delete Account",
+        //             menuItemImage: Assets.delete,
+        //             additionalWidget: Icon(Icons.keyboard_arrow_right_rounded),
+        //           ),
+        //           const Gap(30),
+        //           SizedBox(
+        //             width: DeviceInfo.getDeviceWidth(context),
+        //             child: ElevatedButton(
+        //               onPressed: logout,
+        //               child: const Text("Logout"),
+        //             ),
+        //           ),
+        //           const Gap(100),
+        //         ],
+        //       ),
+        //     ),
+        //   ),
+        // ),
+        );
   }
 }
--- lib/src/features/my profile/features/profile/widgets/my_profile_menu_item.dart
+++ lib/src/features/my profile/features/profile/widgets/my_profile_menu_item.dart
@@ -1,49 +1,65 @@
+import 'package:alhomaidhi_customer_app/src/utils/helpers/device_info.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 
-class MyProfiletMenuItem extends ConsumerWidget {
-  const MyProfiletMenuItem(
-      {super.key,
-      required this.menuItemLink,
-      required this.menuitemName,
-      required this.menuItemImage,
-      required this.additionalWidget});
-
+class MyProfileMenuItem extends StatelessWidget {
+  const MyProfileMenuItem({
+    super.key,
+    required this.menuItemLink,
+    required this.menuitemName,
+    required this.icon,
+    this.isDeleteAccount = false,
+  });
   final String menuItemLink;
   final String menuitemName;
-  final String menuItemImage;
-  final Widget? additionalWidget;
+  final IconData icon;
+  final bool isDeleteAccount;
 
   @override
-  Widget build(context, WidgetRef ref) {
-    return Container(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 0,
-        vertical: 10,
-      ),
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.grey,
-          width: 1,
+  Widget build(BuildContext context) {
+    return InkWell(
+      splashColor: Theme.of(context).colorScheme.onSecondary,
+      borderRadius: BorderRadius.circular(10),
+      onTap: () async {
+        context.push(menuItemLink);
+      },
+      child: Ink(
+        height: DeviceInfo.getDeviceHeight(context) * 0.25,
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                spreadRadius: 2,
+                blurRadius: 5,
+                color: Colors.black.withOpacity(0.3),
+              ),
+            ]),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: DeviceInfo.getDeviceWidth(context) * 0.15,
+              backgroundColor: (isDeleteAccount)
+                  ? Colors.red
+                  : Theme.of(context).scaffoldBackgroundColor,
+              child: Icon(
+                icon,
+                color: (isDeleteAccount)
+                    ? Colors.white
+                    : Theme.of(context).primaryColor,
+                size: 60,
+              ),
+            ),
+            const Gap(10),
+            Text(
+              menuitemName,
+              style: Theme.of(context).textTheme.bodyLarge,
+            )
+          ],
         ),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: ListTile(
-        onTap: () async {
-          // if (menuItemLink == "/address") {
-          //   // ignore: unused_result
-          //   await ref.refresh(addressProvider.future);
-          // }
-          if (!context.mounted) return;
-          context.push(menuItemLink);
-        },
-        leading: Image(
-          image: AssetImage(menuItemImage),
-          width: 33,
-        ),
-        title: Text(menuitemName),
-        trailing: additionalWidget,
       ),
     );
   }
