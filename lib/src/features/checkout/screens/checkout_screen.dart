8fbe9db68c2c748e7ce4136aeb577827a4e64b07
yaseeen96 <syedyaseen1539@gmail.com>
2024-03-18 14:54:18 +0530

    optimize webview for android

--- lib/src/features/checkout/screens/checkout_screen.dart
+++ lib/src/features/checkout/screens/checkout_screen.dart
@@ -1,17 +1,11 @@
 import 'package:Alhomaidhi/src/utils/constants/endpoints.dart';
-import 'package:Alhomaidhi/src/utils/helpers/translation_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-// Import for Android features.
-import 'package:webview_flutter_android/webview_flutter_android.dart';
-// Import for iOS features.
-import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 
 class CheckoutScreen extends ConsumerStatefulWidget {
-  const CheckoutScreen({Key? key, required this.token}) : super(key: key);
-
+  const CheckoutScreen({super.key, required this.token});
   final String token;
 
   @override
@@ -19,132 +13,94 @@ class CheckoutScreen extends ConsumerStatefulWidget {
 }
 
 class _CheckoutScreenState extends ConsumerState<CheckoutScreen> {
-  bool _isLoading = true;
-  final String addIsWebViewScript = '''
-  var url = new URL(window.location.href);
-  if (!url.searchParams.has('iswebView')) {
-    url.searchParams.set('iswebView', 'true');
-    window.location.href = url.toString();
-  };
-''';
-
-  late final PlatformWebViewControllerCreationParams params;
-  late WebViewController controller;
-
-  void initializeController({required String initialUrl}) {
-    controller
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0x00000000))
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onProgress: (int progress) {
-            if (progress >= 90) {
-              setState(() {
-                _isLoading = false;
-              });
-            }
-            debugPrint('WebView is loading (progress : $progress%)');
-          },
-          // error is here - while checking isWebview
-          onPageStarted: (String url) {
-            // Uri uri = Uri.parse(url);
-            // Map<String, dynamic> params = uri.queryParameters;
-
-            // // Logging the initial request URL for debugging.
-            // print("Request URL: ${uri.toString()}");
-            // debugPrint('Page started loading: $url');
-            // if (!params.containsKey('iswebView')) {
-            //   // 'iswebView' not present, add it to the URL
-            //   Uri modifiedUri = uri
-            //       .replace(queryParameters: {...params, 'iswebView': 'true'});
-            //   String modifiedUrl = modifiedUri.toString();
-            //   print("Modified URL to include iswebView: $modifiedUrl");
-            //   controller.loadRequest(modifiedUri);
-            //   // Prevent default loading and use modified URL
-            // }
-          },
-          onPageFinished: (String url) {
-            controller.clearCache();
-            // controller.runJavaScript(addIsWebViewScript);
-
-            // controller.runJavaScript(addIsWebViewScript);
-          },
-          onWebResourceError: (WebResourceError error) {
-            debugPrint('''
-Page resource error:
-  code: ${error.errorCode}
-  description: ${error.description}
-  errorType: ${error.errorType}
-  isForMainFrame: ${error.isForMainFrame}
-          ''');
-          },
-          onNavigationRequest: (NavigationRequest request) {
-            Uri uri = Uri.parse(request.url);
-            Map<String, dynamic> params = uri.queryParameters;
-            // Handling specific navigation cases based on URL.
-            if (uri.path.contains("/order-received")) {
-              context.go("/success");
-              return NavigationDecision.prevent;
-            } else if (uri.path.contains("/cart")) {
-              context.go("/cart");
-              return NavigationDecision.prevent;
-            } else if (uri.path.contains("checkout/") &&
-                params.containsKey('payment_id')) {
-              // This covers both 'tabby failure' and any checkout related failure.
-              context.go("/failure");
-              return NavigationDecision.prevent;
-            }
-            // Add other URL checks and corresponding actions here if needed.
-
-            // 'iswebView' already exists, no modification needed, proceed with navigation
-            return NavigationDecision.navigate;
-          },
-          onUrlChange: (UrlChange change) {
-            debugPrint('url change to ${change.url}');
-          },
-        ),
-      )
-      ..loadRequest(Uri.parse(initialUrl));
+  late WebViewController webViewController;
+  Future<void> clearCookies() async {
+    final CookieManager cookieManager = CookieManager();
+    await cookieManager.clearCookies();
   }
 
   @override
   void initState() {
     super.initState();
-
-    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
-      params = WebKitWebViewControllerCreationParams(
-        allowsInlineMediaPlayback: true,
-        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
-        limitsNavigationsToAppBoundDomains: false,
-      );
-    } else {
-      params = const PlatformWebViewControllerCreationParams();
-    }
-    controller = WebViewController.fromPlatformCreationParams(params);
-
-    final String encodedToken = Uri.encodeComponent(widget.token);
-
-    final String initialUrl =
-        "https://alhomaidhigroup.com/custom-checkout?mo_jwt_token=$encodedToken&iswebView=true";
-    initializeController(initialUrl: initialUrl);
+    clearCookies();
   }
 
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  bool isLoading = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(TranslationHelper.translation(context)!.checkout),
+        title: const Text("Checkout"),
       ),
       body: Stack(
         children: [
-          WebViewWidget(
-            controller: controller,
+          WebView(
+            onWebViewCreated: (controller) {
+              webViewController = controller;
+            },
+            zoomEnabled: false,
+            onWebResourceError: (err) {
+              logger.e("Error: ${err.description}");
+            },
+            onPageFinished: (isFinished) {
+              Future.delayed(const Duration(seconds: 4), () {
+                setState(() {
+                  isLoading = false;
+                });
+              });
+            },
+            navigationDelegate: (request) {
+              String modifiedUrl = request.url;
+
+              // Check if the URL already has query parameters
+              if (modifiedUrl.contains("?")) {
+                modifiedUrl += "&iswebView=true";
+              } else {
+                modifiedUrl += "?iswebView=true";
+              }
+              // HYPERPAY FAILURE URL
+              // https://alhomdelivery.standardtouch.com/checkout/order-pay/1829/?key=wc_order_cVFgG6qogXMWU&callback=true&transaction-key=15054021&id=CAD59C3188A2532C9B997295744AE27A.prod01-vm-tx07&resourcePath=%2Fv1%2Fcheckouts%2FCAD59C3188A2532C9B997295744AE27A.prod01-vm-tx07%2Fpayment
+              // hyperpay processing url
+              // https://alhomaidhigroup.com/checkout/order-pay/6859/?key=wc_order_oddcNYvyYoCIl
+              if (modifiedUrl.contains("/order-received")) {
+                context.go("/success");
+                return NavigationDecision.prevent;
+              }
+              if (modifiedUrl.contains("/cart")) {
+                context.go("/cart");
+                return NavigationDecision.prevent;
+              }
+              // hyperpay failure - issues faced. temp ignored.
+
+              // tabby failure
+              else if (modifiedUrl.contains("checkout/?payment_id")) {
+                context.go("/failure");
+                return NavigationDecision.prevent;
+              }
+              logger.i("request url: $modifiedUrl");
+              webViewController.loadUrl(modifiedUrl);
+              return NavigationDecision
+                  .prevent; // Prevent default behavior since we're manually loading the URL
+
+              // return NavigationDecision.navigate;
+            },
+            javascriptMode: JavascriptMode.unrestricted,
+            initialUrl:
+                "https://alhomaidhigroup.com/custom-checkout?mo_jwt_token=${widget.token}",
           ),
-          if (_isLoading)
+          if (isLoading)
             Container(
               alignment: Alignment.center,
-              color: Colors.white,
-              child: const CircularProgressIndicator(),
+              height: double.infinity,
+              width: double.infinity,
+              decoration: const BoxDecoration(color: Colors.white),
+              child: const SizedBox(
+                  height: 40, width: 40, child: CircularProgressIndicator()),
             ),
         ],
       ),
